befa5619dec9d644716ef816ed8cd8b272b2fd22
khsoo1128 <72716761+khsoo1128@users.noreply.github.com>
2020-11-13 13:11:09 +0900

    java day02 삭제
    java day02 삭제

--- Java/day02/신.java
+++
@@ -1,9 +0,0 @@
-class 신 {
-	public static void main(String args[]){
-		아주머니 아 = new 아주머니();
-		System.out.println("나이가 " + 아.age + "살인 아주머니가 생성되었다"); 
-		아.굽는다();
-		아.판매한다(아.붕1);
-		아.판매한다(아.붕3);
-	}
-}
